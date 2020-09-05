e801faef3b426b231e8a4de5f08798a6db260e93
AlexanderHallCS <hallale21@yahoo.com>
2020-09-05 15:31:15 -0500

    Add in-app web view of FakeNameificator website.

---
+++ Fake Nameificator/WebViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WebViewController.swift
+//  Fake Nameificator
+//
+//  Created by Alexander Hall on 9/5/20.
+//  Copyright © 2020 Hall Inc. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let myURL = URL(string: "https://0e4ef622.github.io/fake-nameificator/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
