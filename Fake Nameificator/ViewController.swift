0caa34aaf7d96401cef1f2926d49a04589ac22dc
AlexanderHallCS <hallale21@yahoo.com>
2020-09-05 15:20:54 -0500

    Initial Commit

---
+++ Fake Nameificator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fake Nameificator
+//
+//  Created by Alexander Hall on 9/5/20.
+//  Copyright © 2020 Hall Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
